ae0174327c89f0174b14e902c551607f7ddaf559
Emirhan Engin <me@emirhanengin.com>
2022-03-29 23:33:54 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+      facere perspiciatis repellat dolorem nemo, fuga, repudiandae beatae saepe
+      recusandae tempora, nihil dignissimos dolor inventore sint perferendis
+      explicabo! Rerum, unde voluptas!
+    </p>
+  )
+}
+
+export default App
